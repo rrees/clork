4dea2d766ef81e5c4cb8f061c45c4c8aa51c1946
Robert Rees <spam@shudderfix.com>
2010-03-09 23:27:00 +0000

    Added a test for the directions in the main namespace so I can try to refactor it. (Proper refactoring)'

--- test/clork/commands_cli_test.clj
+++ test/clork/commands_cli_test.clj
@@ -6,3 +6,9 @@
 	(let [expected-description (:description (:hall rooms))]
 		(is(= expected-description (look)))))
 
+(deftest should-have-compass-directions-as-functions
+	(let [functions (ns-interns 'clork.clork)]
+		(dorun
+		(for [direction ['north 'south 'west 'east]]
+		(is(contains? functions direction ))))))
+
